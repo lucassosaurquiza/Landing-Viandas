2a46742a19bec70775150bec3640a4c196230585
Lucas Sosa <81591140+lucassosaurquiza@users.noreply.github.com>
2023-11-06 20:57:36 -0300

    route-nosotros

---
+++ app/nosotros/page.jsx
@@ -0,0 +1,12 @@
+
+const Nosotros = () => {
+  return (
+    <section>
+      <p className="text-white font-bold hover:text-orange-500 transition duration-300 ease-in-out">
+      Nosotros
+      </p>
+    </section>
+  )
+}
+
+export default Nosotros
