526603a9d8e5f8420d221a869f645c02d693714c
Lucas Sosa <81591140+lucassosaurquiza@users.noreply.github.com>
2023-11-06 20:57:04 -0300

    route-contacto

---
+++ app/contacto/page.jsx
@@ -0,0 +1,11 @@
+
+const Contacto = () => {
+  return (
+    <section>
+    <p className="text-white font-bold hover:text-orange-500 transition duration-300 ease-in-out">Contacto</p>  
+    </section>
+    
+  )
+}
+
+export default Contacto
