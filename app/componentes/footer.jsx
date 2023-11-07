2b04eddca9e8ed7aa4b2069616d72e196dcc677c
Lucas Sosa <81591140+lucassosaurquiza@users.noreply.github.com>
2023-11-07 15:38:31 -0300

    add-footer-component

---
+++ app/componentes/footer.jsx
@@ -0,0 +1,17 @@
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className=" bg-orange-400 flex p-5 justify-around text-xs">
+        <p className="text-white font-bold">
+          Todos los derechos reservados a La Cocina de Edu.
+        </p>
+        <p className="text-white font-bold">
+          Creado por Objeto - Inteligente
+        </p>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
