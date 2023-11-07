22c6476a5365ab3310cededd75836568211a6a70
Lucas Sosa <81591140+lucassosaurquiza@users.noreply.github.com>
2023-11-07 16:32:14 -0300

    add-new-modifies

--- app/page.js
+++ app/page.js
@@ -22,11 +22,11 @@ export default function Home() {
   return (
     <main className="relative flex flex-col items-center justify-center h-screen text-white">
       <img className="w-full h-full absolute object-cover" src="/prueba.jpg" alt="" />
-      <img className="absolute md:top-5 md:right-5 md:h-48 md:w-60 top-5 h-40 w-56 lg:h-24 lg:w-32" src="/logo.png" alt="" />
+      <img className="absolute md:top-5 md:right-5 md:h-48 md:w-60 top-3 h-40 w-56 lg:h-24 lg:w-32" src="/logo.png" alt="" />
       <p className="text-white lg:text-3xl font-bold absolute top-10 left-20 lg:left-1/3 hidden md:block">
         VIANDAS LA COCINA DE EDU
       </p>
-      <p className="text-white text-lg font-bold absolute top-52 block md:hidden">
+      <p className="text-white text-lg font-bold absolute top-44 block md:hidden">
         Viandas a domicilio
       </p>
       <p className=" text-white lg:text-lg font-bold absolute top-20 left-24  lg:left-2/4 hidden md:block">
@@ -37,15 +37,15 @@ export default function Home() {
       </p>
       <BsFillArrowDownCircleFill className=" text-gray-800 text-4xl font-bold absolute top-56 lg:left-1/3 md:top-64 md:left-44 justify-center md:flex hidden" />
       <BsFillArrowDownCircleFill className=" text-gray-800 text-4xl font-bold absolute top-56 lg:right-1/3 md:top-64 md:right-44  justify-center md:flex hidden" />
-      <div className="absolute top-60 md:top-72  md:flex md:gap-10 p-3">
+      <div className="absolute top-52 md:top-72  md:flex md:gap-10 p-3">
         <Link href='/viandas-X1'>
-          <section className="bg-orange-400 p-5 cursor-pointer hover:bg-orange-300 hover:scale-105 transition duration-300 ease-in-out rounded-lg mb-5">
+          <section className="border p-5 cursor-pointer hover:bg-orange-300 hover:scale-105 transition duration-300 ease-in-out rounded-lg mb-5 hover:border-none">
             <div className="border-2 border-gray-800 flex justify-center items-center flex-col h-full mb-5">
-              <h2 className=" lg:text-2xl  font-bold  text-gray-800 ">
-               VIANDAS MENSUALES
+              <h2 className=" lg:text-2xl  font-bold  text-white ">
+               VIANDAS INDIVIDUALES
               </h2>
             </div>
-            <p className="text-white border-white border-2 justify-center flex bg-gray-800 p-2 md:text-xl mb-5">
+            <p className="text-white border-white border-2 justify-center flex bg-gray-800 p-2 md:text-xl ">
               + info del plan
             </p>
             <p className="text-white mb-1 text-xs lg:text-lg hidden md:block">
@@ -63,22 +63,46 @@ export default function Home() {
             <p className="text-white mb-5 text-xs lg:text-lg hidden md:block">
               - Viandas frescas y saludables
             </p>
-            <div className="flex justify-center max-w-">
-              <p className="border md:text-xl font-extrabold flex justify-center p-1 rounded-md w-2/4 bg-orange-100 text-gray-800 text-xs">
-                $5.200
-              </p>
+        
+          </section>
+        </Link>
+
+        <Link href='/viandas-X3'>
+          <section className=" p-5 cursor-pointer hover:bg-amber-800 hover:scale-105 transition duration-300 ease-in-out rounded-lg border hover:border-none mb-5">
+            <div className="border-2 border-gray-800 flex justify-center items-center flex-col h-full mb-5">
+              <h2 className=" lg:text-2xl  font-bold  text-white ">
+                VIANDAS POR 3 PERSONAS
+              </h2>
             </div>
+            <p className="text-white border-white border-2 justify-center flex bg-gray-800 p-2 md:text-xl ">
+              + info del plan
+            </p>
+            <p className="text-white mb-1 text-xs lg:text-lg hidden md:block">
+              - Incluye las 6 comidas diaraias mas sopas y caramelos
+            </p>
+            <p className="text-white mb-1 text-xs lg:text-lg hidden md:block">
+              - Entrega a domicilio entre las 12:00 am. y 14:00 pm.
+            </p>
+            <p className="text-white mb-1 text-xs lg:text-lg hidden md:block">
+              - Plan de 1000, 1200, 1500, y 1800 kcal. diarias
+            </p>
+            <p className="text-white mb-1 text-xs lg:text-lg hidden md:block">
+              - Amplia variedad de platos
+            </p>
+            <p className="text-white mb-5 text-xs lg:text-lg hidden md:block">
+              - Viandas frescas y saludables
+            </p>
           </section>
         </Link>
 
         <Link href='/viandas-X5'>
-          <section className="bg-green-800 p-5 cursor-pointer hover:bg-green-700 hover:scale-105 transition duration-300 ease-in-out rounded-lg">
+          <section className=" p-5 cursor-pointer hover:bg-amber-300 hover:scale-105 transition duration-300 ease-in-out rounded-lg border hover:border-none">
             <div className="border-2 border-gray-800 flex justify-center items-center flex-col h-full mb-5">
-              <h2 className=" lg:text-2xl  font-bold  text-gray-800 ">
+              <h2 className=" lg:text-2xl  font-bold  text-white ">
                 VIANDAS POR 5 PERSONAS
               </h2>
             </div>
-            <p className="text-white border-white border-2 justify-center flex bg-gray-800 p-2 md:text-xl mb-5 ">
+            <p className="text-white border-white border-2 justify-center flex bg-gray-800 p-2 md:text-xl ">
               + info del plan
             </p>
             <p className="text-white mb-1 text-xs lg:text-lg hidden md:block">
@@ -96,11 +120,6 @@ export default function Home() {
             <p className="text-white mb-5 text-xs lg:text-lg hidden md:block">
               - Viandas frescas y saludables
             </p>
-            <div className="flex justify-center max-w-">
-              <p className="border md:text-xl font-extrabold flex justify-center p-1 rounded-md w-2/4 bg-green-100 text-gray-800 text-xs">
-                $8.400
-              </p>
-            </div>
           </section>
         </Link>
       </div>
