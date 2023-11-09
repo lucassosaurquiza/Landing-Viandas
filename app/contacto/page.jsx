0796ccf414cbd7a1055923aff4e164127fa51596
Lucas Sosa <81591140+lucassosaurquiza@users.noreply.github.com>
2023-11-08 21:58:38 -0300

    modifie-contacto

--- app/contacto/page.jsx
+++ app/contacto/page.jsx
@@ -54,21 +54,39 @@ const Contacto = () => {
         </div>
       </header>
 
-      <div className="bg-orange-200">
-        <p className="justify-center flex text-4xl font-bold pb-5 pt-5">
+      <div className="bg-white flex flex-col items-center justify-center min-h-screen">
+        <p className="justify-center flex text-4xl font-bold pb-5 pt-5 text-orange-400">
           Contacto
         </p>
-        <div className="border">
-          <AiFillInstagram />
-          <p>
-            Seguinos
-          </p>
+        <div className="bg-orange-100 p-3 rounded-md">
+         <p className="justify-center flex font-bold mb-5 md:text-xl text-center">
+         Dejanos tu contacto por cualquier consulta que tengas, te respondemos a la brevedad.
+         </p>
+         <div className="md:max-w-4xl">
+         <form action="" className="justify-center  flex-col  mb-5 ">
+          <div className="flex flex-col md:flex">
+          <label className="mr-3">Nombre:</label>
+          <input type="text" name="nombre" placeholder="Nombre" className="rounded-md p-1 mb-2" />
+          <label className="mr-3">Correo:</label>
+          <input type="text" name="correo" placeholder="Correo" className="rounded-md p-1 mb-2" />
+          </div>
+          <div className="flex flex-col">
+          <label className="mr-3">Numero:</label>
+          <input type="text" name="numero" placeholder="Numero" className="rounded-md p-1 mb-2" />
+          <label htmlFor="">Mensaje detallado:</label>
+          <textarea name="" id="" cols="30" rows="10"></textarea>
+          </div>
+         </form>
+         </div>
+         <div className="justify-center flex">
+         <button className="p-2 border border-orange-400 rounded-md text-orange-400 font-bold text-xl hover:shadow-xl transition duration-300 ease-in-out">Enviar</button>
+         </div>
         </div>
       </div>
 
 
 
-      <a className="fixed bottom-5 right-5" href="https://api.whatsapp.com/send?phone=+543794077764&text=Hola,%20quiero%20hacer%20un%20pedido">
+      <a className="fixed bottom-5 right-5" href="https://api.whatsapp.com/send?phone=+543794394705&text=Hola,%20me%20gustaria%20hacer%20una%20consulta">
         <button className="bg-green-500 text-white p-2 rounded-full hover:bg-green-600 transition duration-300 ease-in-out">
           <AiOutlineWhatsApp className="text-4xl" />
         </button>
