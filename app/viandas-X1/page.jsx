39f89bab36431a05a6053154658514486deab6b0
Lucas Sosa <81591140+lucassosaurquiza@users.noreply.github.com>
2023-11-09 13:34:27 -0300

    new-modifies

--- app/viandas-X1/page.jsx
+++ app/viandas-X1/page.jsx
@@ -31,7 +31,7 @@ const ViandasPorUno = () => {
               </Link>
             </div>
             <div className="md:hidden">
-              <Modal/>
+              <Modal />
             </div>
             <div className="md:flex items-center gap-10 hidden ">
 
@@ -57,50 +57,68 @@ const ViandasPorUno = () => {
       </header>
 
 
-      <div className="border flex justify-center text-center">
-        <div className="max-w-3xl border border-blue-600 mt-10">
-          <h2 className="text-4xl font-bold flex justify-center mb-10">Lo mejor en viandas mensuales</h2>
+      <div className="border flex justify-center text-center ">
+        <div className="max-w-3xl mt-10 mb-10">
+          <h2 className="text-4xl font-extrabold flex justify-center mb-10 text-orange-500">LO MEJOR EN VIANDAS INDIVIDUALES</h2>
           <p className="flex justify-center text-center mb-5">
             Unite a los cientos de clientes que mejoraron su salud con viandas La Cocina de Edu.
             Depende tus objetivos puedes usar nuestras deliciosas comidas para tener una dieta saludable que ayude a bajar de peso o mantenerte con mucha energía durante todo el dia.
-            Recorda que con nuestras viandas tendrás un plato diferente cada día y semana. Atendemos de Lunes a Sabados.
+            Recorda que con nuestras viandas tendrás un plato diferente cada día y semana. Atendemos de Lunes a Viernes.
           </p>
-          <div className="border flex flex-col items-center mb-5">
-            <div className="border bg-orange-200 p-5">
-              <p className="font-bold text-3xl mb-3">
+          <h2 className="flex justify-center text-center mb-5 font-bold md:text-3xl">
+            Informate de lo que podes encontrar en nuestro menu individual a continuacion:
+          </h2>
+          <div className=" flex flex-col items-center mb-5">
+            <div className="border bg-orange-300 p-5">
+              <p className="font-bold md:text-3xl mb-3 text-white">
                 Esta opcion incluye
               </p>
 
-              <p className="mb-2">
-                - 6 comidas diarias mas sopas y caramelos
+              <p className="text-white mb-1 text-xs lg:text-lg  font-bold">
+                - Incluye porcion abundante con pan.
               </p>
-              <p className="mb-2">
+              <p className="text-white mb-1 text-xs lg:text-lg  font-bold">
                 - Entrega a domicilio entre las 12:00 am. y 14:00 pm.
               </p>
-              <p className="mb-2">
-                - Plan de 1000, 1200, 1500, y 1800 kcal. diarias
+              <p className="text-white mb-1 text-xs lg:text-lg  font-bold" >
+                - 20 dias de vianda.
               </p>
-              <p className="mb-2">
-                - Amplia variedad de platos
+              <p className="text-white mb-1 text-xs lg:text-lg  font-bold">
+                - Tenes tu menu en feriados regionales.
               </p>
-              <p className="mb-2">
-                - Viandas frescas y saludables
+              <p className="text-white mb-5  text-xs lg:text-lg  font-bold">
+                - Te corremos el dia en feriados nacionales.
+              </p>
+              <p className="text-white  border-white border-2 justify-center flex bg-gray-800 p-2 md:text-md mb-3">
+                + info del plan
               </p>
             </div>
           </div>
-          <h2 className="text-lg md:text-4xl font-bold flex justify-center mb-5">Estos son algunos de nuestros platos de la semana</h2>
-          <div className="flex justify-center gap-5">
-            <div className="flex-col flex justify-center">
-              <p className="text-red-400 font-semibold mb-3">
-                Menu comun  /  Milanesa con ensalada mixta
+          <h2 className="text-lg md:text-4xl font-bold flex justify-center mb-5">Algunos de nuestros menus de la semana</h2>
+          <div className="md:flex  justify-center gap-5 p-3">
+            <div className="flex-col flex justify-center items-center">
+              <p className="text-orange-400 font-semibold mb-3">
+                /  Milanesa con ensalada mixta
               </p>
-              <img className="md:h-40 h-32" src="/milanesa.png" alt="" />
+              <img className="md:h-40 h-24 w-52" src="/milanesa.png" alt="" />
             </div>
-            <div className="flex-col flex justify-center">
-              <p className="text-red-400 font-semibold mb-3">
-                Menu especial  /  Milanesa con ensalada mixta
+            <div className="flex-col flex justify-center items-center">
+              <p className="text-orange-400 font-semibold mb-3">
+             /  Pollo Principe 
               </p>
-              <img className="md:h-40 h-32" src="/milanesa.png" alt="" />
+              <img className="md:h-40 h-24 w-52" src="/pollo-principe.jpg" alt="" />
+            </div>
+            <div className="flex-col flex justify-center items-center">
+              <p className="text-orange-400 font-semibold mb-3">
+             /  Pollo al horno
+              </p>
+              <img className="md:h-40 h-24 w-52" src="/pollo-al-horno.jpg" alt="" />
+            </div>
+            <div className="flex-col flex justify-center items-center">
+              <p className="text-orange-400 font-semibold mb-3">
+             /  Pollo Principe
+              </p>
+              <img className="md:h-40 h-24 w-52" src="/pollo-principe.jpg" alt="" />
             </div>
           </div>
         </div>
