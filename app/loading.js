9d83ff77d30e05b1ff66bd0841b2995faf081a3d
Lucas Sosa <81591140+lucassosaurquiza@users.noreply.github.com>
2023-11-08 21:59:23 -0300

    modifie-loading

--- app/loading.js
+++ app/loading.js
@@ -1,3 +1,11 @@
-export default function Loading(){
-  return <div>Cargando...</div>
+export default function Loading() {
+  return <div className="flex justify-center items-center h-screen relative">
+    <div className="w-32 h-32 border-8 border-orange-700 rounded-full animate-ping">
+      <div className="w-32 h-32 border-8 border-orange-700 rounded-full animate-ping"></div>
+    </div>
+    <div className="w-32 h-32 border-8 border-orange-400 rounded-full animate-pulse absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"></div>
+    <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center z-10 p-3 border bg-orange-400 rounded-lg">
+      <img className="h-12" src="/nuevo-logo.png" alt="" />
+    </div>
+  </div>
 }
