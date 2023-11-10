3eb705b1cf2aa1574f5693140772b5b22e47ef6a
Lucas Sosa <81591140+lucassosaurquiza@users.noreply.github.com>
2023-11-10 12:09:35 -0300

    new-component

---
+++ app/componentes/loadingSpinner.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const LoadingSpinner = () => {
+  return (
+    <div className="w-10 h-10 border-t-4 border-orange-500 border-t-orange-500 border-b-0 border-r-0 border-l-5 rounded-full animate-spin">
+    </div>
+  );
+};
+
+export default LoadingSpinner;
