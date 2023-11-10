20698566c92fea931286f92b10fb7b802cca6b6f
Lucas Sosa <81591140+lucassosaurquiza@users.noreply.github.com>
2023-11-10 12:13:10 -0300

    new-modifies

--- app/componentes/disclosure.jsx
+++ app/componentes/disclosure.jsx
@@ -54,7 +54,7 @@ export default function Example() {
                 leaveTo="transform scale-95 opacity-0"
               >
                 <Disclosure.Panel className="px-4 my-3 text-gray-800">
-                  Siempre tenemos al menos 2 platos diferentes de menu por si uno no te gusta.
+                  Siempre tenemos otra alternativa diferente de menu por si uno no te gusta.
                 </Disclosure.Panel>
               </Transition>
 
@@ -85,6 +85,35 @@ export default function Example() {
               >
                 <Disclosure.Panel className="px-4 my-3 text-gray-800">
                Si, aceptamos metodo de pago en transferencia y efectivo.
+                </Disclosure.Panel>
+              </Transition>
+
+              https://github.com/lucassosaurquiza
+            </>
+          )}
+        </Disclosure>
+
+        <Disclosure>
+          {({ open }) => (
+            <>
+              <Disclosure.Button className="flex w-full justify-between rounded-lg bg-orange-100 px-4 py-2 mb-5 text-left text-sm font-medium text-orange-900 hover:bg-orange-200 transition duration ease-in-out">
+                <span className='text-gray-800'>¿La entrega a domicilio es con cargo?</span>
+                <ChevronUpIcon
+                  className={`${open ? 'rotate-180 transform' : ''
+                    } h-5 w-5 text-orange-500`}
+                />
+              </Disclosure.Button>
+
+              <Transition
+                enter="transition duration-300 ease-in-out"
+                enterFrom="transform scale-95 opacity-0"
+                enterTo="transform scale-300 opacity-300"
+                leave="transition duration-75 ease-in-out"
+                leaveFrom="transform scale-300 opacity-300"
+                leaveTo="transform scale-95 opacity-0"
+              >
+                <Disclosure.Panel className="px-4 my-3 text-gray-800">
+                  No, la entrega a domicilio es totalmente gratuita.
                 </Disclosure.Panel>
               </Transition>
 
