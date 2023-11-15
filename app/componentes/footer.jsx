cede67543b2f3715502348ba9a816a715ea8d020
Lucas Sosa <81591140+lucassosaurquiza@users.noreply.github.com>
2023-11-15 10:46:35 -0300

    modifies-footer

--- app/componentes/footer.jsx
+++ app/componentes/footer.jsx
@@ -2,7 +2,7 @@
 const Footer = () => {
   return (
     <footer>
-      <div className=" bg-amber-700 flex p-5 justify-around text-xs">
+      <div className=" bg-orange-400 flex p-5 justify-around text-xs">
         <p className="text-white font-bold">
         © 2023 La Cocina de Edu - Todos los derechos reservados
         </p>
