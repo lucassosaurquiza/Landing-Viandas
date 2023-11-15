30ebdc68dacba82febf75914ae518277a95834e8
Lucas Sosa <81591140+lucassosaurquiza@users.noreply.github.com>
2023-11-15 11:34:09 -0300

    add-meta-google

--- app/layout.js
+++ app/layout.js
@@ -12,6 +12,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <meta name="google-site-verification" content="mkmZgkKy3ddAGhZcUOmiE_2CrA-y-eqJ8L5C-EBQjFo" />
       <body className={Garamound.className}>{children}</body>
       <Footer/>
     </html>
